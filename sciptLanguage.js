692aa3ef5edaae96e94c2b1c2e43a456b1d2f075
AlwaysBeHappy0420 <leeweixuan19@gmail.com>
2026-01-13 17:33:31 +0800

    Chinese Scripting

--- sciptLanguage.js
+++ sciptLanguage.js
@@ -50,7 +50,7 @@ const translations = {
     info :"My Info",
     hero_title: "Hello, This is <span>Lee Wei Xuan</span>",
     hero_desc:
-      "I am a game developer, and you’ve come to the right place if you’re looking for a strong and reliable one.",
+      "I am a Malaysian game developer, and you’ve come to the right place if you’re looking for a strong and reliable one.",
     view_work: "View My Work",
     about_title: "About Me",
     about_desc: 
@@ -58,11 +58,11 @@ const translations = {
     +"Holding <a href=\"https://study.utar.edu.my/game-development.php\" target=\"_blank\">Bachelor of Science(Honours) Game Development</a><b>(CPGA 3.2 out of 4)</b> from Universiti Tunku Abdul Rahman in Selangor, Malaysia.<br><br>"
     +"<h3>Working Experiences:</h3><br>"
     +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-    +"- <i>Worked in industry as an intern from June2025 to Oct2025</i>"
+    +"- <i>Worked in <b>Inverse Atelier</b> as a <b>Game Programmer Intern</b> from June2025 to Oct2025</i>"
     + "<br><br>"
-    
+
     +"<h3>These describe me well:</h3> "
-    +"<b><h3>Skillsets:</h3></b><br>"
+    +"<b><h4>Skillsets:</h4></b><br>"
     +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
     +"・I love <b>Unity</b>, experienced in 2D/3D C# Coding, including <b>Shader Programming, Profiling, Debugging, Optimization, Networking</b>.<br>"
     +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
@@ -73,10 +73,12 @@ const translations = {
     +"・I love & hate <b>Dev Logging</b>. Don't get me wrong. I'm good at concluding things. It just takes me a lot of time.<br>"
     +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
     +"・<b>Communicating</b> is a skill. TBH, I'm not good at this because of most of the time I would prefer solve it by myself first, and in the end I can solve it by myself.<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・Learned <b>Japanese(N1). Native in English, Chinese(Simplified/Traditional), Malay, Hakka, Cantonese, some Hokkien.</b>"
     +"<br>"
 
     +"&nbsp;&nbsp;&nbsp;&nbsp;"
-    +"<b><h3>Mindsets:</h3></b><br>"
+    +"<b><h4>Mindsets:</h4></b><br>"
     +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
     +"・I do <b>enjoy the process of developing a game</b>, since it always gives me a sense \"Someone is actually using/playing what I have done\", and I enjoy it so much.<br>"
     +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
@@ -100,11 +102,46 @@ const translations = {
     info:"个人资料",
     hero_title: "这里是 <span>李维旋</span> 的个人网站",
     hero_desc:
-      "我是一名游戏开发者，如果你正在寻找一位有充足实力、值得信赖的游戏开发人员，这里是正确的选择。",
+      "我是一名马来西亚的游戏开发者，如果你正在寻找一位有充足实力、值得信赖的游戏开发人员，这里是正确的选择。",
     view_work: "查看我的作品",
     about_title: "关于我",
     about_desc:
-      "我是一名热爱创造独特且有趣体验的开发者，专注于 Unity、游戏设计与网页开发。",
+    "这是我，李维旋经由Github正经营着的 <b>个人网站</b>。 这个网站目前处于<a href=\"https://github.com/AlwaysBeHappy0420/AlwaysBeHappy_LeeWeiXuan\" target=\"_blank\">开源</a>状态, 倘若需要，可以直接下载。<br>"
+    +"持有<a href=\"https://study.utar.edu.my/game-development.php\" target=\"_blank\">Bachelor of Science(Honours) Game Development（游戏开发）</a><b>(CPGA 3.2 out of 4)</b> 的文凭，毕业于 Universiti Tunku Abdul Rahman，位于Selangor, Malaysia.<br><br>"
+    +"<h3>工作经历:</h3><br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"- <i>在<b>Inverse Atelier</b> 担任<b>实习游戏开发人员</b>，时期：2025年6月 至 2025年 10月</i>"
+    + "<br><br>"
+
+    +"<h3>个人特质:</h3> "
+    +"<b><h4>技能:</h4></b><br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・专精于<b>Unity</b>, 可熟悉运用 2D以及3D C# Coding, 包括但不限于 <b>Shader Programming, Profiling, Debugging, Optimization, Networking</b>进行游戏开发。<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・有使用<b>虚幻引擎(Unreal Engine)</b> 的经验, 可使用<b>Blueprint</b> & <b>C/C++</b>进行游戏开发。<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・有制作 <b>个人网站</b>的经验。熟悉使用以下语言： <b>JavaScript, HTML, CSS, php, MySQL...</b><br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・熟悉写<b>开发日志</b>。<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・不擅长与人进行口头表达上的<b>沟通</b>，但文字表达上相对流畅。<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・获得<b>日语(N1)准证. 母语为英文，中文（简体/繁体），马来文，客家语，粤语，一部分福建话（闽南语）</b>"
+    +"<br>"
+
+    +"&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"<b><h4>思想:</h4></b><br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・享受 <b>开发游戏的过程</b>， 因为它总能给我提供一种成就感。<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・个人口头禅： &nbsp;&nbsp;<b><i>如果一样事情可以被描述，那么它就可以被程序编写。</i></b><br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・抱有 <b>对新事物的好奇心</b>，因其总能让我认识新的世界。<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・抱有 <b>对事物追根究底的精神</b>，因其总能让我理解事物运转的逻辑。<br>"
+    +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+    +"・在<b>压力下也能有良好表现</b>，也绝<b>不会延迟事物的完成日期</b>。<br>"
+    ,
     projects_title: "项目",
     project1_title: "AXERAS",
     project1_desc: "实习时期参与的项目",
