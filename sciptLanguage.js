939bb2b9a0af91767db095d2cacfc0d88d4e684a
AlwaysBeHappy0420 <leeweixuan19@gmail.com>
2026-01-16 12:55:02 +0800

    small changes

--- sciptLanguage.js
+++ sciptLanguage.js
@@ -414,19 +414,19 @@ const translations = {
     about_desc3:
       "<h3>These describe me well:</h3> " +
       "<b><h4>Skillsets:</h4></b><br>" +
-      "・I love <b>Unity</b>, experienced in 2D/3D C# Coding, including <b>Shader Programming, Profiling, Debugging, Optimization, Networking</b>.<br><br>" +
-      "・I love <b>Unreal</b> as well, not that much compared to Unity, but have experiences with <b>Blueprint</b> & <b>C/C++</b>.<br><br>" +
-      "・I <b>make websites</b>. Not very detailed, but enough to show what I want to show. Using languages like: <b>JavaScript, HTML, CSS, php, MySQL...</b><br><br>" +
-      "・I love & hate <b>Dev Logging</b>. Don't get me wrong. I'm good at concluding things. It just takes me a lot of time.<br><br>" +
-      "・<b>Communicating</b> is a skill. TBH, I'm not good at this because of most of the time I would prefer solve it by myself first, and in the end I can solve it by myself.<br><br>" +
-      "・Learned <b>Japanese(N1). Native in English, Chinese(Simplified/Traditional), Malay, Hakka, Cantonese, some Hokkien.</b><br><br><br>",
+      "・I love <b><i>Unity</i></b>, experienced in 2D/3D C# Coding, including <b><i>Shader Programming, Profiling, Debugging, Optimization, Networking</i></b>.<br><br>" +
+      "・I love <b><i>Unreal</i></b> as well, not that much compared to Unity, but have experiences with <b><i>Blueprint</i></b> & <b><i>C/C++</i></b>.<br><br>" +
+      "・I <b><i>make websites</i></b>. Not very detailed, but enough to show what I want to show. Using languages like: <b><i>JavaScript, HTML, CSS, php, MySQL...</i></b><br><br>" +
+      "・I love & hate <b><i>Dev Logging</i></b>. Don't get me wrong. I'm good at concluding things. It just takes me a lot of time.<br><br>" +
+      "・<b><i>Communicating</i></b> is a skill. TBH, I'm not good at this because of most of the time I would prefer solve it by myself first, and in the end I can solve it by myself.<br><br>" +
+      "・Learned <b><i>Japanese(N1). Native in English, Chinese(Simplified/Traditional), Malay, Hakka, Cantonese, some Hokkien.</i></b><br><br><br>",
     about_desc4:
       "<b><h4>Mindset:</h4></b><br>" +
-      '・I do <b>enjoy the process of developing a game</b>, since it always gives me a sense "Someone is actually using/playing what I have done", and I enjoy it so much.<br><br>' +
+      '・I do <b><i>enjoy the process of developing a game</i></b>, since it always gives me a sense "Someone is actually using/playing what I have done", and I enjoy it so much.<br><br>' +
       "・A hot take from me: &nbsp;&nbsp;<b><i>If one is describable, one is scriptable.</i></b><br><br>" +
-      "・I am <b>curious in new things</b> which leads me to know more about the world.<br><br>" +
-      '・I would <b>like to know "why" behind every question</b> and task which leads me to have high understanding of the item.<br><br>' +
-      "・I can <b>work well under pressure</b>, and I'm <b>concious of time.</b><br><br>",
+      "・I am <b><i>curious in new things</i></b> which leads me to know more about the world.<br><br>" +
+      '・I would <b><i>like to know "why" behind every question</i></b> and task which leads me to have high understanding of the item.<br><br>' +
+      "・I can <b><i>work well under pressure</i></b>, and I'm <b><i>concious of time.</i></b><br><br>",
     projects_title: "Projects",
     project1_title: "AXERAS",
     project1_desc: "Project participated during internship",
@@ -438,10 +438,10 @@ const translations = {
     "<b>Passed</b> - Unity Basics: Animator, C#Scripts, Basic Math implemented into Unity.<br>" +
     "<b>Passed</b> - C/C++ basics, HTML, CSS, JavaScript, PHP, MySQL.<br>" +
     "<b>Passed</b> - Animation Principles, Game Physics, DSA, Basic 3D Model.<br>" +
-    "<b>Passed</b> - Mobile Game Development.<br>" +
+    "<b>Passed</b> - UnrealEngine Blueprint, Mobile Game Development.<br>" +
     "<b>Passed</b> - Design Pattern, Unity Renderer Feature basics, Shader Graph, AI for games.<br>" +
     "<b>Passed</b> - Internship.<br>" +
-    "<b>On-Going</b> - Now improving my skills on Unity HLSL, Renderer Feature, and UE C/C++"
+    "<b>On-Going</b> - Improving my skills on Unity HLSL, Renderer Feature, and UE C/C++"
   },
 
   zh: {
@@ -461,19 +461,19 @@ const translations = {
     about_desc3:
       "<h3>个人特质:</h3> " +
       "<b><h4>技能:</h4></b><br>" +
-      "・专精于<b>Unity</b>, 可熟悉运用 2D以及3D C# Coding, 包括但不限于 <b>Shader Programming, Profiling, Debugging, Optimization, Networking</b>进行游戏开发。<br><br>" +
-      "・有使用<b>虚幻引擎(Unreal Engine)</b> 的经验, 可使用<b>Blueprint</b> & <b>C/C++</b>进行游戏开发。<br><br>" +
-      "・有制作 <b>个人网站</b>的经验。熟悉使用以下语言： <b>JavaScript, HTML, CSS, php, MySQL...</b><br><br>" +
-      "・熟悉写<b>开发日志</b>。<br><br>" +
-      "・不擅长与人进行口头表达上的<b>沟通</b>，但文字表达上相对流畅。<br><br>" +
-      "・获得<b>日语(N1)准证. 母语为英文，中文（简体/繁体），马来文，客家语，粤语，一部分福建话（闽南语）</b><br><br><br>",
+      "・专精于<b><i>Unity</i></b>, 可熟悉运用 2D以及3D C# Coding, 包括但不限于 <b><i>Shader Programming, Profiling, Debugging, Optimization, Networking</i></b>进行游戏开发。<br><br>" +
+      "・有使用<b><i>虚幻引擎(Unreal Engine)</i></b> 的经验, 可使用<b><i>Blueprint</i></b> & <b><i>C/C++</i></b>进行游戏开发。<br><br>" +
+      "・有制作 <b><i>个人网站</i></b>的经验。熟悉使用以下语言： <b><i>JavaScript, HTML, CSS, php, MySQL...</i></b><br><br>" +
+      "・熟悉写<b><i>开发日志</i></b>。<br><br>" +
+      "・不擅长与人进行口头表达上的<b><i>沟通</i></b>，但文字表达上相对流畅。<br><br>" +
+      "・获得<b><i>日语(N1)准证. 母语为英文，中文（简体/繁体），马来文，客家语，粤语，一部分福建话（闽南语）</i></b><br><br><br>",
     about_desc4:
       "<b><h4>个人思想:</h4></b><br>" +
-      "・享受 <b>开发游戏的过程</b>， 因为它总能给我提供一种成就感。<br><br>" +
+      "・享受 <b><i>开发游戏的过程</i></b>， 因为它总能给我提供一种成就感。<br><br>" +
       "・个人口头禅： &nbsp;&nbsp;<b><i>如果一样事情可以被描述，那么它就可以被程序编写。</i></b><br><br>" +
-      "・抱有 <b>对新事物的好奇心</b>，因其总能让我认识新的世界。<br><br>" +
-      "・抱有 <b>对事物追根究底的精神</b>，因其总能让我理解事物运转的逻辑。<br><br>" +
-      "・在<b>压力下也能有良好表现</b>，也绝<b>不会延迟事物的完成日期</b>。<br><br>",
+      "・抱有 <b><i>对新事物的好奇心</i></b>，因其总能让我认识新的世界。<br><br>" +
+      "・抱有 <b><i>对事物追根究底的精神</i></b>，因其总能让我理解事物运转的逻辑。<br><br>" +
+      "・在<b><i>压力下也能有良好表现</i></b>，也绝<b><i>不会延迟事物的完成日期</i></b>。<br><br>",
     projects_title: "项目",
     project1_title: "AXERAS",
     project1_desc: "实习时期参与的项目",
@@ -485,7 +485,7 @@ const translations = {
     "<b>已学习</b> - Unity 基础：Animator，C# 脚本，基础数学在 Unity 中的应用。<br>" +
     "<b>已学习</b> - C/C++ 基础，HTML，CSS，JavaScript，PHP，MySQL。<br>" +
     "<b>已学习</b> - 动画原理，游戏物理，数据结构与算法，基础 3D 建模。<br>" +
-    "<b>已学习</b> - 手机游戏开发。<br>" +
+    "<b>已学习</b> - 虚幻引擎Blueprint，手机游戏开发。<br>" +
     "<b>已学习</b> - 脚本设计模式，Unity Renderer Feature 基础，Shader Graph，游戏 AI。<br>" +
     "<b>已学习</b> - 实习。<br>" +
     "<b>进行中</b> - 目前正在提升 Unity HLSL、Renderer Feature 以及 UE C/C++ 的技能"
@@ -509,21 +509,21 @@ const translations = {
     about_desc3:
       "<h3>個人特質:</h3> " +
       "<b><h4>スキル:</h4></b><br>" +
-      "・<b>Unity</b> を専門とし、2Dおよび3DのC#コーディングに精通しています。<b>シェーダープログラミング、プロファイリング、デバッグ、最適化、ネットワーク</b> などを含むゲーム開発が可能です。<br><br>" +
-      "・<b>Unreal Engine</b> の使用経験があり、<b>Blueprint</b> および <b>C/C++</b> を用いたゲーム開発が可能です。<br><br>" +
-      "・<b>個人サイト</b> の制作経験があります。以下の言語の使用に精通しています： <b>JavaScript, HTML, CSS, php, MySQL...</b><br><br>" +
-      "・<b>開発ログ</b> の作成に慣れています。<br><br>" +
-      "・口頭での<b>コミュニケーション</b>はあまり得意ではありませんが、文章での表現は比較的得意です。<br><br>" +
-      "・<b>日本語(N1)資格取得。母語は英語、中国語（簡体字／繁体字）、マレー語、客家語、広東語、一部福建語（閩南語）</b><br><br><br>",
+      "・<b><i>Unity</i></b> を専門とし、2Dおよび3DのC#コーディングに精通しています。<b><i>シェーダープログラミング、プロファイリング、デバッグ、最適化、ネットワーク</i></b> などを含むゲーム開発が可能です。<br><br>" +
+      "・<b><i>Unreal Engine</i></b> の使用経験があり、<b><i>Blueprint</i></b> および <b><i>C/C++</i></b> を用いたゲーム開発が可能です。<br><br>" +
+      "・<b><i>個人サイト</i></b> の制作経験があります。以下の言語の使用に精通しています： <b><i>JavaScript, HTML, CSS, php, MySQL...</i></b><br><br>" +
+      "・<b><i>開発ログ</i></b> の作成に慣れています。<br><br>" +
+      "・口頭での<b><i>コミュニケーション</i></b>はあまり得意ではありませんが、文章での表現は比較的得意です。<br><br>" +
+      "・<b><i>日本語(N1)資格取得。母語は英語、中国語（簡体字／繁体字）、マレー語、客家語、広東語、一部福建語（閩南語）</i></b><br><br><br>",
     about_desc4:
       "<b><h4>個人の考え:</h4></b><br>" +
-      "・<b>ゲーム開発の過程を心から楽しんでおり</b>、一つ一つの制作工程が大きな達成感につながっています。<br><br>" +
+      "・<b><i>ゲーム開発の過程を心から楽しんでおり</i></b>、一つ一つの制作工程が大きな達成感につながっています。<br><br>" +
       "・<b><i>「記述可能なものは、すべてプログラムによって実現できる。」</i></b>と個人の考えです。<br><br>" +
-      "・<b>常に新しい物事に好奇心を持ち</b>、それを通して新しい世界や価値観を学び続けています。<br><br>" +
-      "・<b>物事を徹底的に追究する姿勢を強みとしており</b>、常に「なぜそう動くのか」という論理を理解することを大切にしています。。<br><br>" +
-      "・<b>プレッシャーのかかる状況でも安定したパフォーマンスを発揮でき、期限を守ることを徹底しています。</b>。<br><br>",
+      "・<b><i>常に新しい物事に好奇心を持ち</i></b>、それを通して新しい世界や価値観を学び続けています。<br><br>" +
+      "・<b><i>物事を徹底的に追究する姿勢を強みとしており</i></b>、常に「なぜそう動くのか」という論理を理解することを大切にしています。。<br><br>" +
+      "・<b><i>プレッシャーのかかる状況でも安定したパフォーマンスを発揮でき、期限を守ることを徹底しています。</i></b><br><br>",
     projects_title: "プロジェクト",
-    project1_title: "アクスラス（AXERAS）",
+    project1_title: "AXERAS（アックセラス）",
     project1_desc: "インターンシップ期間中に開発に参加したプロジェクト",
     project2_title: "Virtual Escape（ヴァーチャル・エスケープ）",
     project2_desc: "大学卒業前に制作したゲーム",
@@ -533,7 +533,7 @@ const translations = {
     "<b>勉強済み</b> - Unity 基礎：Animator、C# スクリプト、Unity における基礎数学の実装。<br>" +
     "<b>勉強済み</b> - C/C++ 基礎、HTML、CSS、JavaScript、PHP、MySQL。<br>" +
     "<b>勉強済み</b> - アニメーション原理、ゲーム物理、データ構造とアルゴリズム、基本的な 3D モデリング。<br>" +
-    "<b>勉強済み</b> - モバイルゲーム開発。<br>" +
+    "<b>勉強済み</b> - UE Blueprint、モバイルゲーム開発。<br>" +
     "<b>勉強済み</b> - デザインパターン、Unity Renderer Feature 基礎、Shader Graph、ゲーム AI。<br>" +
     "<b>勉強済み</b> - インターンシップ。<br>" +
     "<b>進行中</b> - Unity HLSL、Renderer Feature、および UE C/C++ のスキル向上に取り組んでいます"
